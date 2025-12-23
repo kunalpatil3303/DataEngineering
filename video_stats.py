778c88a19c461ad0934659fa4e496bcd14c45940
KUNAL <kunalpatil3303@gmail.com>
2025-12-23 13:49:14 -0500

    Added get_video_ids

--- video_stats.py
+++ video_stats.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 def get_playlist_id():
 
@@ -30,5 +31,46 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
     
+
+def get_video_ids(playlist_id):
+
+    pageToken = None
+    video_ids = []
+
+    base_url = (
+        f"https://youtube.googleapis.com/youtube/v3/playlistItems"
+        f"?part=contentDetails"
+        f"&maxResults={maxResults}"
+        f"&playlistId={playlist_id}"
+        f"&key={API_KEY}"
+    )
+
+    try:
+        while True:
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)
+
+            pageToken = data.get("nextPageToken")
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+    
 if __name__ == "__main__":
-    get_playlist_id()
+    playlist_id = get_playlist_id()
+    get_video_ids(playlist_id)
